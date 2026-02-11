ff513f5d981295253a488be8b9f21ef66079350a
jeffh90 <jeffhalley420@gmail.com>
2026-02-11 12:01:25 +0000

    add scafolding for ec2 instance for cloud database solution. complete terraform configureation yet to be implemented

--- main.tf
+++ main.tf
@@ -45,6 +45,12 @@ resource "aws_lambda_function" "api_handler" {
   timeout = 30
   memory_size = 512
 
+  vpc_config {
+  subnet_ids         = data.aws_subnets.default.ids
+  security_group_ids = [aws_security_group.lambda_sg.id]
+}
+
+
   environment {
     variables = {
       MODEL_ID = "eu.amazon.nova-lite-v1:0"
@@ -166,7 +172,6 @@ resource "aws_apigatewayv2_stage" "default" {
   }
 }
 
-
 ############################################
 # 9. Allow API Gateway to invoke Lambda
 ############################################
@@ -178,9 +183,134 @@ resource "aws_lambda_permission" "apigw_lambda" {
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
 
+
+############################################
+# data sources
+############################################
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "al2023_arm" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-6.1-arm64"]
+  }
+}
+
+#Security Group: Postgres
+
+resource "aws_security_group" "postgres" {
+  name        = "postgres-spot-sg"
+  description = "Postgres access from Lambda"
+  vpc_id      = data.aws_vpc.default.id
+
+  # TEMP SSH (remove after setup)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR.IP.ADDRESS/32"]
+  }
+
+  # Postgres
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lambda_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# lambda security Group
+
+resource "aws_security_group" "lambda_sg" {
+  name        = "lambda-to-postgres-sg"
+  description = "Lambda outbound to Postgres"
+  vpc_id      = data.aws_vpc.default.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# ec2 spot instance
+
+resource "aws_instance" "postgres" {
+  ami           = data.aws_ami.al2023_arm.id
+  instance_type = "t4g.small"
+  subnet_id     = data.aws_subnets.default.ids[0]
+
+  vpc_security_group_ids = [aws_security_group.postgres.id]
+
+  key_name = "placeholder"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -e
+
+    dnf update -y
+    dnf install -y postgresql16-server
+
+    /usr/bin/postgresql-setup --initdb
+
+    sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf
+    echo "host all all 0.0.0.0/0 scram-sha-256" >> /var/lib/pgsql/data/pg_hba.conf
+
+    systemctl enable postgresql
+    systemctl start postgresql
+  EOF
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      spot_instance_type               = "persistent"
+      instance_interruption_behavior   = "stop"
+    }
+  }
+
+  tags = {
+    Name = "postgres-spot"
+  }
+}
+
+
+
+
 ############################################
 # Output
 ############################################
 output "api_endpoint" {
   value = "${aws_apigatewayv2_api.http_api.api_endpoint}/ask"
+}
+
+output "postgres_private_ip" {
+  value = aws_instance.postgres.private_ip
 }
